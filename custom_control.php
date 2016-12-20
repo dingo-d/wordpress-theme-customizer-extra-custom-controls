db3e4607b1883c0e339f5c0804404ec3f4c3ae2c
Denis Žoljom <dingo-d@users.noreply.github.com>
2016-12-20 10:01:53 +0100

    Updated to version 1.2.0
    Added slider control, fixed the image radio button control, fixed according to coding standards, added comments.

--- custom_control.php
+++ custom_control.php
@@ -1,101 +1,229 @@
 <?php
+/**
+ * Customizer custom controls
+ *
+ * @package WordPress
+ * @subpackage /inc/customizer
+ * @version 1.1.0
+ * @author  Denis Žoljom <http://madebydenis.com/>
+ * @license https://www.gnu.org/licenses/gpl-3.0.txt GNU/GPLv3
+ * @link https://github.com/dingo-d/wordpress-theme-customizer-extra-custom-controls
+ * @since  1.0.0
+ *
+ * Be sure to include your custom-controls.css file and
+ * custom-control.js file.
+ * Modify the path of the css and js files according to where
+ * you put your custom controls and customizer settings.
+ */
 
-if (class_exists('WP_Customize_Control')){
+$my_theme = wp_get_theme();
+$theme_version = $my_theme->get( 'Version' );
 
-/* Custom checkbox switch */
+if ( class_exists( 'WP_Customize_Control' ) ) {
 
-/* Be sure to include your custom_controls.css file. Modify the path of the css file according to where you put your custom controls and customizer settings */
-
-	class Toggle_Checkbox_Custom_control extends WP_Customize_Control{
+	/**
+	 * Checkbox toggle custom control
+	 *
+	 * @package WordPress
+	 * @subpackage inc/customizer
+	 * @version 1.1.0
+	 * @author  Denis Žoljom <http://madebydenis.com/>
+	 * @license https://www.gnu.org/licenses/gpl-3.0.txt GNU/GPLv3
+	 * @link https://github.com/dingo-d/wordpress-theme-customizer-extra-custom-controls
+	 * @since  1.0.0
+	 */
+	class Checkbox_Custom_Control extends WP_Customize_Control {
+		/**
+		 * Control type
+		 *
+		 * @var string
+		 */
 		public $type = 'toogle_checkbox';
-		public function enqueue(){
-			wp_enqueue_style( 'custom_controls_css', get_template_directory().'/custom_controls_css.css');
+		/**
+		 * Control scripts and styles enqueue
+		 *
+		 * @since 1.0.0
+		 */
+		public function enqueue() {
+			wp_enqueue_script( 'custom_controls', get_template_directory_uri() . '/inc/customizer/js/custom-controls.js', array( 'jquery' ), $theme_version, true );
+			wp_enqueue_style( 'custom_controls_css', get_template_directory_uri() . '/inc/customizer/css/custom-controls.css' );
 		}
-		public function render_content(){
+		/**
+		 * Control method
+		 *
+		 * @since 1.0.0
+		 */
+		public function render_content() {
 			?>
 			<div class="checkbox_switch">
 				<div class="onoffswitch">
-				    <input type="checkbox" id="<?php echo esc_attr($this->id); ?>" name="<?php echo esc_attr($this->id); ?>" class="onoffswitch-checkbox" value="<?php echo esc_attr( $this->value() ); ?>" <?php $this->link(); checked( $this->value() ); ?>>
-				    <label class="onoffswitch-label" for="<?php echo esc_attr($this->id); ?>"></label>
+				    <input type="checkbox" id="<?php echo esc_attr( $this->id ); ?>" name="<?php echo esc_attr( $this->id ); ?>" class="onoffswitch-checkbox" value="<?php echo esc_attr( $this->value() ); ?>" <?php $this->link(); ?> <?php $this->link() . checked( $this->value() ); ?>>
+				    <label class="onoffswitch-label" for="<?php echo esc_attr( $this->id ); ?>"></label>
 				</div>
 				<span class="customize-control-title onoffswitch_label"><?php echo esc_html( $this->label ); ?></span>
-				<p><?php echo wp_kses_post($this->description); ?></p>
+				<p><?php echo esc_html( $this->description ); ?></p>
 			</div>
 			<?php
 		}
 	}
 
-/* Custom Info */
-
-	class Info_Custom_control extends WP_Customize_Control{
+	/**
+	 * Info custom control
+	 *
+	 * @package WordPress
+	 * @subpackage inc/customizer
+	 * @version 1.1.0
+	 * @author  Denis Žoljom <http://madebydenis.com/>
+	 * @license https://www.gnu.org/licenses/gpl-3.0.txt GNU/GPLv3
+	 * @link https://github.com/dingo-d/wordpress-theme-customizer-extra-custom-controls
+	 * @since  1.0.0
+	 */
+	class Info_Custom_Control extends WP_Customize_Control {
+		/**
+		 * Control type
+		 *
+		 * @var string
+		 */
 		public $type = 'info';
-		public function render_content(){
+		/**
+		 * Control method
+		 *
+		 * @since 1.0.0
+		 */
+		public function render_content() {
 			?>
 			<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
-			<p><?php echo wp_kses_post($this->description); ?></p>
+			<p><?php echo esc_html( $this->description ); ?></p>
 			<?php
 		}
 	}
 
-/* Custom Separator */
-
-	class Separator_Custom_control extends WP_Customize_Control{
+	/**
+	 * Separator custom control
+	 *
+	 * @package WordPress
+	 * @subpackage inc/customizer
+	 * @version 1.1.0
+	 * @author  Denis Žoljom <http://madebydenis.com/>
+	 * @license https://www.gnu.org/licenses/gpl-3.0.txt GNU/GPLv3
+	 * @link https://github.com/dingo-d/wordpress-theme-customizer-extra-custom-controls
+	 * @since  1.0.0
+	 */
+	class Separator_Custom_Control extends WP_Customize_Control {
+		/**
+		 * Control type
+		 *
+		 * @var string
+		 */
 		public $type = 'separator';
-		public function render_content(){
+		/**
+		 * Control method
+		 *
+		 * @since 1.0.0
+		 */
+		public function render_content() {
 			?>
 			<p><hr></p>
 			<?php
 		}
 	}
 
-/* Multi Input field */
-
-	class Multi_Input_Custom_control extends WP_Customize_Control{
+	/**
+	 * Multi input custom control
+	 *
+	 * @package WordPress
+	 * @subpackage inc/customizer
+	 * @version 1.1.0
+	 * @author  Denis Žoljom <http://madebydenis.com/>
+	 * @license https://www.gnu.org/licenses/gpl-3.0.txt GNU/GPLv3
+	 * @link https://github.com/dingo-d/wordpress-theme-customizer-extra-custom-controls
+	 * @since  1.0.0
+	 */
+	class Multi_Input_Custom_Control extends WP_Customize_Control {
+		/**
+		 * Control type
+		 *
+		 * @var string
+		 */
 		public $type = 'multi_input';
-		public function enqueue(){
-			wp_enqueue_script( 'custom_controls', get_template_directory().'/custom_controls_js.js', array( 'jquery' ),'', true );
-			wp_enqueue_style( 'custom_controls_css', get_template_directory().'/custom_controls_css.css');
+		/**
+		 * Control scripts and styles enqueue
+		 *
+		 * @since 1.0.0
+		 */
+		public function enqueue() {
+			wp_enqueue_script( 'custom_controls', get_template_directory_uri() . '/inc/customizer/js/custom-controls.js', array( 'jquery' ), $theme_version, true );
+			wp_enqueue_style( 'custom_controls_css', get_template_directory_uri() . '/inc/customizer/css/custom-controls.css' );
 		}
-		public function render_content(){
+		/**
+		 * Control method
+		 *
+		 * @since 1.0.0
+		 */
+		public function render_content() {
 			?>
 			<label class="customize_multi_input">
 				<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
-				<p><?php echo wp_kses_post($this->description); ?></p>
-				<input type="hidden" id="<?php echo esc_attr($this->id); ?>" name="<?php echo esc_attr($this->id); ?>" value="<?php echo esc_attr($this->value()); ?>" class="customize_multi_value_field" data-customize-setting-link="<?php echo esc_attr($this->id); ?>"/>
+				<p><?php echo esc_html( $this->description ); ?></p>
+				<input type="hidden" id="<?php echo esc_attr( $this->id ); ?>" name="<?php echo esc_attr( $this->id ); ?>" value="<?php echo esc_attr( $this->value() ); ?>" class="customize_multi_value_field" <?php $this->link(); ?> />
 				<div class="customize_multi_fields">
 					<div class="set">
 						<input type="text" value="" class="customize_multi_single_field"/>
-						<a href="#" class="customize_multi_remove_field">X</a>
+						<a href="#" class="customize_multi_remove_field"><span class="dashicons dashicons-no-alt"></span></a>
 					</div>
 				</div>
-				<a href="#" class="button button-primary customize_multi_add_field"><?php esc_attr_e('Add More', 'your_theme') ?></a>
+				<a href="#" class="button button-primary customize_multi_add_field"><?php esc_html_e( 'Add More', 'utter' ) ?></a>
 			</label>
 			<?php
 		}
 	}
 
-/* Sidebar Dropdown field */
-
-	class Sidebar_Dropdown_Custom_Control extends WP_Customize_Control{
+	/**
+	 * Sidebar dropdown custom control
+	 *
+	 * @package WordPress
+	 * @subpackage inc/customizer
+	 * @version 1.1.0
+	 * @author  Denis Žoljom <http://madebydenis.com/>
+	 * @license https://www.gnu.org/licenses/gpl-3.0.txt GNU/GPLv3
+	 * @link https://github.com/dingo-d/wordpress-theme-customizer-extra-custom-controls
+	 * @since  1.0.0
+	 */
+	class Sidebar_Dropdown_Custom_Control extends WP_Customize_Control {
+		/**
+		 * Control type
+		 *
+		 * @var string
+		 */
 		public $type = 'sidebar_dropdown';
-		public function enqueue(){
-			wp_enqueue_script( 'custom_controls', get_template_directory().'/custom_controls_js.js', array( 'jquery' ),'', true );
-			wp_enqueue_style( 'custom_controls_css', get_template_directory().'/custom_controls_css.css');
+		/**
+		 * Control scripts and styles enqueue
+		 *
+		 * @since 1.0.0
+		 */
+		public function enqueue() {
+			wp_enqueue_script( 'custom_controls', get_template_directory_uri() . '/inc/customizer/js/custom-controls.js', array( 'jquery' ), $theme_version, true );
+			wp_enqueue_style( 'custom_controls_css', get_template_directory_uri() . '/inc/customizer/css/custom-controls.css' );
 		}
-	    public function render_content(){
+		/**
+		 * Control method
+		 *
+		 * @since 1.0.0
+		 */
+		public function render_content() {
 		?>
 			<label class="customize_dropdown_input">
 				<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
-				<p><?php echo wp_kses_post($this->description); ?></p>
+				<p><?php echo esc_html( $this->description ); ?></p>
 				<?php
 				global $wp_registered_sidebars;
-				 ?>
-				<select id="<?php echo esc_attr($this->id); ?>" name="<?php echo esc_attr($this->id); ?>" data-customize-setting-link="<?php echo esc_attr($this->id); ?>">
+					?>
+				<select id="<?php echo esc_attr( $this->id ); ?>" name="<?php echo esc_attr( $this->id ); ?>" <?php $this->link(); ?> >
 				<?php
 				$sidebar_shop = $wp_registered_sidebars;
-				if(is_array($sidebar_shop) && !empty($sidebar_shop)){
-					foreach($sidebar_shop as $sidebar){
-						echo '<option value="'.$sidebar['name'].'" ' . selected( $this->value(), $sidebar['name'], false ) . '>'.$sidebar['name'].'</option>';
+				if ( is_array( $sidebar_shop ) && ! empty( $sidebar_shop ) ) {
+					foreach ( $sidebar_shop as $sidebar ) {
+						echo '<option value="' . esc_attr( $sidebar['name'] ) . '" ' . selected( $this->value(), $sidebar['name'], false ) . '>' . esc_html( $sidebar['name'] ) . '</option>';
 					}
 				}
 				?>
@@ -103,39 +231,102 @@ if (class_exists('WP_Customize_Control')){
 				<br>
 			</label>
 		<?php
-	    }
+		}
 	}
 
-/* Image Select field */
-
-/* You need to include the images you want to select. Add as many as you want to. In this example there are 3 images, that are supposed to go in 3 columns in one row.*/
-
-	class Image_Select_Custom_Control extends WP_Customize_Control{
-		public $type = 'image_select';
-		public function enqueue(){
-			wp_enqueue_style( 'custom_controls_css', get_template_directory().'/custom_controls_css.css');
+	/**
+	 * Image layout picker custom control
+	 *
+	 * @package WordPress
+	 * @subpackage inc/customizer
+	 * @version 1.1.0
+	 * @author  Denis Žoljom <http://madebydenis.com/>
+	 * @license https://www.gnu.org/licenses/gpl-3.0.txt GNU/GPLv3
+	 * @link https://github.com/dingo-d/wordpress-theme-customizer-extra-custom-controls
+	 * @since  1.0.0
+	 */
+	class Image_Radio_Buttons extends WP_Customize_Control {
+		/**
+		 * Control type
+		 *
+		 * @var string
+		 */
+		public $type = 'image_radio_buttons';
+		/**
+		 * Control scripts and styles enqueue
+		 *
+		 * @since 1.0.0
+		 */
+		public function enqueue() {
+			wp_enqueue_style( 'custom_controls_css', get_template_directory_uri() . '/inc/customizer/css/custom-controls.css' );
 		}
-		public function render_content(){
+		/**
+		 * Control method
+		 *
+		 * @since 1.0.0
+		 */
+		public function render_content() {
 		?>
-			<div class="customize_image_select">
+			<div class="image_radio_buttons">
 				<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
-				<p><?php echo wp_kses_post($this->description); ?></p>
+				<p><?php echo esc_html( $this->description ); ?></p>
+				<?php
+				foreach ( $this->choices as $choices_key => $choices_value ) {
+					?>
 					<label>
-						<input type="radio" name="<?php echo esc_attr($this->id); ?>" value="1" data-customize-setting-link="<?php echo esc_attr($this->id); ?>" <?php checked('1', esc_attr($this->value()) );?>/>
-						<img src="<?php echo get_template_directory().'/images/image_1.png'?>" alt="<?php esc_attr_e('Option 1', 'your_theme'); ?>" title="<?php esc_attr_e('Option 1', 'your_theme'); ?>" />
+						<input type="radio" name="<?php echo esc_attr( $this->id ); ?>" value="<?php echo esc_attr( $choices_key ); ?>" <?php $this->link(); ?> <?php checked( esc_attr( $choices_key ), $this->value() ); ?>/>
+						<img src="<?php echo esc_attr( $choices_value['image'] ); ?>" alt="<?php esc_attr( $choices_value['name'] ); ?>" title="<?php esc_attr( $choices_value['name'] ); ?>" />
 					</label>
-					<label>
-						<input type="radio" name="<?php echo esc_attr($this->id); ?>" value="2" data-customize-setting-link="<?php echo esc_attr($this->id); ?>" <?php checked('2', esc_attr($this->value()) );?>/>
-						<img src="<?php echo get_template_directory().'/images/image_2.png'?>" alt="<?php esc_attr_e('Option 2', 'your_theme'); ?>" title="<?php esc_attr_e('Option 2', 'your_theme'); ?>" />
-					</label>
-					<label>
-						<input type="radio" name="<?php echo esc_attr($this->id); ?>" value="3" data-customize-setting-link="<?php echo esc_attr($this->id); ?>" <?php checked('3', esc_attr($this->value()) );?>/>
-						<img src="<?php echo get_template_directory().'/images/image_3.png'?>" alt="<?php esc_attr_e('Option 3', 'your_theme'); ?>"  title="<?php esc_attr_e('Option 3', 'your_theme'); ?>" />
-					</label>
+					<?php
+				}
+				?>
 			</div>
 		<?php
 		}
 	}
 
+	/**
+	 * Slider custom control
+	 *
+	 * @package WordPress
+	 * @subpackage inc/customizer
+	 * @version 1.1.0
+	 * @author  Denis Žoljom <http://madebydenis.com/>
+	 * @license https://www.gnu.org/licenses/gpl-3.0.txt GNU/GPLv3
+	 * @link https://github.com/dingo-d/wordpress-theme-customizer-extra-custom-controls
+	 * @since  1.0.0
+	 */
+	class Slider_Control extends WP_Customize_Control {
+		/**
+		 * Control type
+		 *
+		 * @var string
+		 */
+		public $type = 'slider_control';
+		/**
+		 * Control scripts and styles enqueue
+		 *
+		 * @since 1.0.0
+		 */
+		public function enqueue() {
+			wp_enqueue_script( 'custom_controls', get_template_directory_uri() . '/inc/customizer/js/custom-controls.js', array( 'jquery', 'jquery-ui-slider' ), $theme_version, true );
+			wp_enqueue_style( 'custom_controls_css', get_template_directory_uri() . '/inc/customizer/css/custom-controls.css' );
+		}
+		/**
+		 * Control method
+		 *
+		 * @since 1.0.0
+		 */
+		public function render_content() {
+		?>
+			<div class="slider_control">
+				<span class="customize-control-title"><?php echo esc_html( $this->label ); ?><span class="slider_value"><?php echo esc_attr( $this->value() ); ?></span></span>
+				<p><?php echo esc_html( $this->description ); ?></p>
+				<input class="slider_input" type="hidden" name="<?php echo esc_attr( $this->id ); ?>" value="<?php echo esc_attr( $this->value() ); ?>" <?php $this->link(); ?>  />
+				<div class="slider-range"></div>
+			</div>
+		<?php
+		}
+	}
 
 }
